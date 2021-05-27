773fc3ea3d4871aa666187b02cf79417cde4c573
VDS13 <55276080+VDS13@users.noreply.github.com>
2021-05-27 22:56:49 +0300

    to 11

--- my.sql
+++ my.sql
@@ -1,4 +1,5 @@
 ### 1 ###
+### SQL Basics: Simple JOIN with COUNT(7 kyu) ###
 ###For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return the COUNT of the toys###
 SELECT p.id, p.name, COUNT(t.id) AS toy_count
 FROM people p
@@ -6,6 +7,7 @@ LEFT JOIN toys t ON t.people_id = p.id
 GROUP BY p.id, p.name
 
 ### 2 ###
+### SQL Basics: Top 10 customers by total payments amount(6 kyu) ###
 ###Your are working for a company that wants to reward its top 10 customers with a free gift. You have been asked to generate a simple report that returns the top 10 customers by total amount spent ordered from highest to lowest. Total number of payments has also been requested.###
 SELECT p.customer_id, c.email, COUNT(p.payment_id) AS payments_count , CAST (SUM(p.amount) AS FLOAT) AS total_amount
 FROM payment p
@@ -14,6 +16,7 @@ GROUP BY p.customer_id, c.email
 ORDER BY total_amount DESC LIMIT 10
 
 ### 3 ###
+### SQL Basics: Simple FULL TEXT SEARCH(6 kyu) ###
 ###For this challenge you need to create a simple SELECT statement. Your task is to create a query and do a FULL TEXT SEARCH. You must search the product table on the field name for the word Awesome and return each row with the given word. Your query MUST contain to_tsvector and to_tsquery PostgreSQL functions.###
 SELECT * FROM
   product
@@ -21,6 +24,7 @@ WHERE
   to_tsvector(name) @@ to_tsquery('Awesome')
 
 ### 4 ###
+### Easy SQL: Absolute Value and Log to Base(7 kyu) ###
 ###Return a table with two columns (abs, log) where the values in abs are the absolute values of number1 and the values in log are values from number2 in logarithm to base 64.###
 SELECT
   ABS(number1) AS abs,
@@ -28,11 +32,13 @@ SELECT
 FROM decimals
 
 ### 5 ###
+### SQL Basics: Simple EXISTS(6 kyu) ###
 ###For this challenge you need to create a SELECT statement that will contain data about departments that had a sale with a price over 98.00 dollars. This SELECT statement will have to use an EXISTS to achieve the task.###
 SELECT * FROM departments d
   WHERE EXISTS (SELECT * FROM sales WHERE department_id = d.id AND price > 98)
 
 ### 6 ###
+### SQL Basics: Simple VIEW(5 kyu) ###
 ###For this challenge you need to create a VIEW. This VIEW is used by a sales store to give out vouches to members who have spent over $1000 in departments that have brought in more than $10000 total ordered by the members id. The VIEW must be called members_approved_for_voucher then you must create a SELECT query using the view.###
 CREATE VIEW members_approved_for_voucher AS
   SELECT m.id, m.name, m.email, SUM(p.price) AS total_spending FROM members m
@@ -48,6 +54,7 @@ CREATE VIEW members_approved_for_voucher AS
  SELECT * FROM members_approved_for_voucher;
 
 ### 7 ###
+### SQL: Regex AlphaNumeric Split(6 kyu) ###
 ###Your job is to split out the letters and numbers from the address provided, and return a table in the following format.###
 SELECT  project,
         REGEXP_REPLACE(address, '\d', '', 'g') AS letters,
@@ -56,6 +63,7 @@ FROM
         repositories
 
 ### 8 ###
+### Calculating Running Total(5 kyu) ###
 ###Given a posts table that contains a created_at timestamp column write a query that returns date (without time component), a number of posts for a given date and a running (cumulative) total number of posts up until a given date. The resulting set should be ordered chronologically by date..###
 SELECT  tmp.date_s AS date,
         tmp.count,
@@ -68,6 +76,7 @@ GROUP BY CAST(p.created_at AS DATE)
 ORDER BY CAST(p.created_at AS DATE)) tmp
 
 ### 9 ###
+### Count IP Addresses(5 kyu) ###
 ###Given a database of first and last IPv4 addresses, calculate the number of addresses between them (including the first one, excluding the last one).###
 CREATE FUNCTION ip2int(text) RETURNS bigint AS $$
 SELECT split_part($1,'.',1)::bigint*16777216 + split_part($1,'.',2)::bigint*65536 +
@@ -75,4 +84,24 @@ SELECT split_part($1,'.',1)::bigint*16777216 + split_part($1,'.',2)::bigint*6553
 $$ LANGUAGE SQL  IMMUTABLE RETURNS NULL ON NULL INPUT;
 
 SELECT id, (ip2int(last) - ip2int(first)) AS ips_between FROM ip_addresses;
+
+### 10 ###
+### Grasshopper - Check for factor(8 kyu) ###
+###This function should test if the factor is a factor of base. Return true if it is a factor or false if it is not.###
+SELECT
+  id,
+  CASE
+    WHEN base % factor = 0
+      THEN true
+      ELSE FALSE
+  END res
+FROM kata
+
+### 11 ###
+### SQL Basics: Group By Day(5 kyu) ###
+###There is an events table used to track different key activities taken on a website. For this task you need to filter the name field to only show "trained" events. Events should be grouped by the day they happened and counted. The description field is used to distinguish which items the events happened on.###
+SELECT CAST(created_at AS DATE) AS day, description, COUNT(*) FROM events
+  WHERE name LIKE 'trained'
+  GROUP BY description, CAST(created_at AS DATE)
+  ORDER BY day
 
